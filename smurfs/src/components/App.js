8f661770264468f54c28adf38874f130a6e092c7
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-20 12:04:45 -0400

    context built and axios call works

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import SmurfsForm from "./SmurfsForm";
 import SmurfsList from "./SmurfsList";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Smurfs with context api</h1>
+import { SmurfContext } from "../context/smurfContext";
+import axios from "axios";
+
+const App = () => {
+  const [smurfData, setSmurfData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log(response.data);
+        setSmurfData(response.data);
+      })
+      .catch(error => console.log("u messed up", error));
+  }, []);
+
+  const addSmurf = smurf => {
+    setSmurfData([...smurfData, smurf]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Smurfs with context api</h1>
+      <SmurfContext.Provider value={{ setSmurfData, smurfData, addSmurf }}>
         <SmurfsForm />
         <SmurfsList />
-      </div>
-    );
-  }
-}
+      </SmurfContext.Provider>
+    </div>
+  );
+};
 
 export default App;
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,13 +1,6 @@
 import React from "react";
-import axios from "axios";
 
 const SmurfsList = () => {
-  axios
-    .get("http://localhost:3333/smurfs")
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(err => console.log("you messed up", err));
   return (
     <div>
       <p>wow</p>
