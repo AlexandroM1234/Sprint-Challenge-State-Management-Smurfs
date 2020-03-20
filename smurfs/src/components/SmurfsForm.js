1a155b2901ccd96d3b74cc2839d19e4870519c19
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-20 11:16:02 -0400

    Components built and rendering

---
+++ smurfs/src/components/SmurfsForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SmurfsForm = () => {
+  return (
+    <div>
+      <p>SmurfsForm</p>
+    </div>
+  );
+};
+
+export default SmurfsForm;
---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SmurfsList = () => {
+  return (
+    <div>
+      <p>SmurfsList</p>
+    </div>
+  );
+};
+
+export default SmurfsList;
